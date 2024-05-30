eca257f4a348d813c72033a178af954964989fe8
unknown <ranjbaramir829@gmail.com>
2024-05-30 12:52:25 -0700

    change dockerfile and runprojcet with daphne

--- car_ads/settings.py
+++ car_ads/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = [
     'daphne',
     "haystack",
     'drf_spectacular',
-    "admin_persian",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
