3146bb8881c561c89f649bd5fbd8eb26a8c0f972
unknown <ranjbaramir829@gmail.com>
2024-05-31 02:24:39 -0700

    remove cors pacage

--- car_ads/asgi.py
+++ car_ads/asgi.py
@@ -7,7 +7,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from chat.routing import websocket_urlpatterns
-
+from channels.security.websocket import OriginValidator
 
 
 
@@ -17,8 +17,11 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        "websocket": OriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(websocket_urlpatterns)
+            ),
+            ['*']
         ),
     }
 )
